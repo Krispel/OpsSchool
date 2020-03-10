aee1aca9f1d82ef3b1c8cb124e9f2a82e56e41d9
Krispel <57794007+Krispel@users.noreply.github.com>
2020-03-10 18:18:01 +0200

    partial solution
    hope its ok.
    solved 1-3 but for some reason i didnt manage to run the scripts while the machine is boot.
    thank you for the opportunity!

--- vagrant/fixScripts/exercise2-fix.sh
+++ vagrant/fixScripts/exercise2-fix.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 #add fix to exercise2 here
+sudo chmod 777 /etc/hosts
+sudo cp /etc/hosts /etc/hosts.tmp
+sudo sed '$ d' /etc/hosts.tmp > /etc/hosts
+sudo rm -f /etc/hosts.tmp
+
--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 #add fix to exercise3 here
+
+TARGET="/etc/apache2/sites-available/default"
+TMPFILE="/etc/apache2/sites-available/default.tmp"
+sudo chmod 777 $TARGET
+sudo cp $TARGET /etc/apache2/sites-available/default.tmp
+sudo sed 's/Deny/Allow/g' $TMPFILE > $TARGET 
+sudo rm -f $TMPFILE 
+sudo chmod 777 $TARGET
+
+
